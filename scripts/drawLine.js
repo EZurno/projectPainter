c2855ccf6bd96aba3299ac383c36cd72cdb1823f
Lee Junmo <ezurno@gmail.com>
2022-11-28 18:46:38 +0900

    DAY 4
    1. drawLines, colorPicker
    
    2. colorOption은 배열로 받음 -> htmlCollection[]으로 주기 때문
    
    3. ctx.beginPath()로 신규 선과 기존 선의 경로를 끊어주어야 연동이 안됨
    
    4. event.target.dataset.color

--- scripts/drawLine.js
+++ scripts/drawLine.js
@@ -1,9 +1,15 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.querySelector("#line_width");
+const color = document.querySelector("#color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color_option")
+);
+//colorOption은 배열로 받아야 됨, color-option이 htmlCollection[]으로 주기 때문
 
 canvas.width = 600;
 canvas.height = 600;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -13,6 +19,8 @@ function onMove(event) {
     ctx.stroke();
     return;
   }
+  ctx.beginPath(); // 새로그린 선과 다음에 그리는 선의 경로를 끊어줘야 함.
+
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -24,7 +32,29 @@ function cancelPainting() {
   isPainting = false;
 }
 
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  //console.dir(event.target.dataset.color);
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
